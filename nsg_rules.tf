d51174289184dc10a0b231c7ded07603735c6bfe
satyasai1460 <strikersatya2@gmail.com>
2024-02-26 16:40:12 +0530

    Added NSG Rules 80 and 443

---
+++ nsg_rules.tf
@@ -0,0 +1,27 @@
+resource "azurerm_network_security_rule" "nsgrule80" {
+  name                        = "test123"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "80"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "nsgrule443" {
+  name                        = "nsgrule443"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "443"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
+}
