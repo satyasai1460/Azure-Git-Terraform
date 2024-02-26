5faa1259304bd6cb6a333541ead13fefd0168c2d
satyasai1460 <strikersatya2@gmail.com>
2024-02-26 22:27:51 +0530

    added terraform.tfvars

--- variables.tf
+++ variables.tf
@@ -1,31 +1,31 @@
 variable "rg_name" {
-  type    = string
-  default = "rg"
+  type = string
+  #   default = "rg"
 }
 
 variable "location" {
-  type    = string
-  default = "eastus"
+  type = string
+  #   default = "eastus"
 }
 
 variable "env" {
-  type    = string
-  default = "Development"
+  type = string
+  #   default = "Development"
 }
 
 variable "VNet" {
-  type    = string
-  default = "Devops-Vnetwork"
+  type = string
+  #   default = "Devops-Vnetwork"
 }
 
 variable "VNet_cidr" {
-  type    = string
-  default = "10.35.0.0/16"
+  type = string
+  #   default = "10.35.0.0/16"
 }
 
 variable "VNet_dns" {
-  type    = list(any)
-  default = ["8.8.8.8", "8.8.4.4"]
+  type = list(any)
+  #   default = ["8.8.8.8", "8.8.4.4"]
 }
 
 
